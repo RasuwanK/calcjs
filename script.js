3b8ffc7ddff4ac2bb0ba1f0841c4b5bee7762054
Rasuwan Kalhara <49600817+RasuwanK@users.noreply.github.com>
2021-09-08 23:57:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+
+// Written by rasuwank
+
+
+// Array containing characters in the screen
+screenText = [];
+
+
+// Function converts an array into string
+const makeString = (arr) => {
+    let str = '';
+    for(let char of arr){
+        str += char;
+    }
+
+    return str;
+}
+
+
+const render = (screen) => {
+    screen.innerHTML = makeString(screenText);
+}
+
+// Screen of the calculator
+const screen = document.getElementById('screen');
+
+
+// Group of buttons
+const numberButtons = document.getElementsByClassName('number');
+
+const functionButtons = document.getElementsByClassName('func');
+
+const clearButton = document.getElementById('ce');
+
+const equalButton = document.getElementById('eq');
+
+// Cleaning the screen
+clearButton.addEventListener('click',() => {
+    screenText = [];
+    screen.innerHTML = '0';
+})
+
+equalButton.addEventListener('click',() => {
+
+    //if(screen.innerHTML.indexOf('//') == -1 || screen.innerHTML.indexOf('/*') == -1){
+        try{
+            screen.innerHTML = eval(screen.innerHTML);
+            screenText = [];
+
+        }catch(e){
+            screen.innerHTML = "Malformed expression";
+            screenText = [];
+        }
+});
+
+// Asigning all the functions for number buttons
+for(let element of numberButtons){
+    element.addEventListener("click", () => {
+        screenText.push(element.id);
+        render(screen);
+    })
+}
+
+for(let element of functionButtons){
+    element.addEventListener("click" , () => {
+        screenText.push(element.id);
+        render(screen);
+    })
+}
+
+
